ac7f7ba0591a42f109da1c272feb54341b21ba83
zahid4kh <halilzahid@gmail.com>
2024-10-12 21:39:28 +0200

    function floatRange not needed anymore, i am respawning food inside of onDraw scope; speed set to 1 for tests

--- app/src/main/java/basic/pacman/Variables.kt
+++ app/src/main/java/basic/pacman/Variables.kt
@@ -8,6 +8,18 @@ val arrowRight = R.drawable.arrow_right
 val arrowLeft = R.drawable.arrow_left
 val buttonSize = 90.dp
 
-const val movingSpeed = 5f
+const val movingSpeed = 1f
 const val spawnPosX = -50f
-const val spawnPosY = -10f
+const val spawnPosY = -50f
+
+//
+//fun floatRange(start: Float, endInclusive: Float, step: Float = 5.0f): List<Float> {
+//    require(step > 0) { "Step must be positive." }
+//    val range = mutableListOf<Float>()
+//    var current = start
+//    while (current <= endInclusive) {
+//        range.add(current)
+//        current += step
+//    }
+//    return range
+//}
