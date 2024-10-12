d7ef52cec16b9d62e71001cb113d681fb02c9789
zahid4kh <halilzahid@gmail.com>
2024-10-12 02:20:03 +0200

    -

--- app/src/main/java/basic/pacman/Counter.kt
+++ app/src/main/java/basic/pacman/Counter.kt
@@ -10,13 +10,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun ScoreCount(score: String) {
+    val context = LocalContext.current
+    val screenDensityDpi = remember { getScreenDensityDpi(context) }
+
     Spacer (modifier = Modifier.height(40.dp))
     Row(modifier = Modifier
         .fillMaxWidth()
@@ -26,6 +31,6 @@ fun ScoreCount(score: String) {
         horizontalArrangement = Arrangement.Center){
 
         Text(text = "SCORE: ", fontSize = 35.sp, color = Color.White)
-        Text(text = score, fontSize = 35.sp, color = Color.White)
+        Text(text = screenDensityDpi.toString(), fontSize = 35.sp, color = Color.White)
     }
 }
