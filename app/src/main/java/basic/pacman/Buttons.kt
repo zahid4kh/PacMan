e3bc360f981af78cb235afb82d4eff3552393fe4
zahid4kh <halilzahid@gmail.com>
2024-10-12 02:22:15 +0200

    file for buttonIcons

---
+++ app/src/main/java/basic/pacman/Buttons.kt
@@ -0,0 +1,9 @@
+package basic.pacman
+
+import androidx.compose.ui.unit.dp
+
+val arrowUp = R.drawable.arrow_up
+val arrowDown = R.drawable.arrow_down
+val arrowRight = R.drawable.arrow_right
+val arrowLeft = R.drawable.arrow_left
+val buttonSize = 90.dp
